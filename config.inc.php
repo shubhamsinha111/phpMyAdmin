8d9072c4713f51998b5d49ad14cd011affb0f530
shubhamsinha111 <33378014+shubhamsinha111@users.noreply.github.com>
2020-06-26 00:08:51 +0530

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -32,6 +32,8 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['user'] = 'user'; //mysql username here
+$cfg['Servers'][$i]['password'] = 'password'; //mysql password here
 
 /**
  * phpMyAdmin configuration storage settings.
